4bd296ed0240d6ebae741a7e3c45cd88e7f91c2f
SlendGamer <timpru895@gmail.com>
2022-06-09 14:40:33 +0200

    add device classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,103 @@
 #include "main.h"
 
 void setup() {
-  pinMode(RELAY_1, OUTPUT);
-  pinMode(RELAY_2, OUTPUT);
-  pinMode(RELAY_3, OUTPUT);
-  pinMode(RELAY_4, OUTPUT);
 
-  pinMode(HUM_SENSOR_1, INPUT);
-  pinMode(HUM_SENSOR_2, INPUT);
-  pinMode(HUM_SENSOR_3, INPUT);
-  pinMode(HUM_SENSOR_4, INPUT);
+  Pump pump1;
+  pump1.pin = 1;
+  pump1.activeLevel = HIGH_ACTIVE;
 
-  Valve valve1;
-  valve1.valveNum = 0;
-  valve1.valvePin = 1;
-  valve1.init();
-  
-  Zone zone1;
-  zone1.zoneNum = 0;
+  Valve valve1, valve2;
+  valve1.pin = 2;
+  valve1.activeLevel = HIGH_ACTIVE;
+  valve2.pin = 3;
+  valve2.activeLevel = HIGH_ACTIVE;
 
-  Sensor sensor1;
-  sensor1.sensorNum = 0;
-  sensor1.init();
+  Zone zone1, zone2;
+
+  Sensor sensor1, sensor2;
+
+// pointer references
+// children
+  waterIO.pumps = new Pump*[1] {&pump1};
+  waterIO.valves = new Valve*[2] {&valve1, &valve2};
+  waterIO.zones = new Zone*[2] {&zone1, &zone2};
+  waterIO.sensors = new Sensor*[2] {&sensor1, &sensor2};
+
+  pump1.valves = new Valve*[2] {&valve1, &valve2};
+
+  valve1.zones = new Zone*[1] {&zone1};
+  valve2.zones = new Zone*[1] {&zone2};
+
+  zone1.sensors = new Sensor*[1] {&sensor1};
+  zone2.sensors = new Sensor*[1] {&sensor2};
+
+// parents
+  valve1.pump = &pump1;
+  valve2.pump = &pump1;
+
+  zone1.valve = &valve1;
+  zone2.valve = &valve2;
+
+  sensor1.zone = &zone1;
+  sensor2.zone = &zone2;
+
+// initilization
+uint8_t arrayLength = (sizeof(*waterIO.pumps)/sizeof(**waterIO.pumps)) + 1;
+for(uint8_t i = 0; i < arrayLength; i++) {
+  waterIO.pumps[i];
+}
 
 
 }
 
 void loop() {
-  uint8_t currentZoneHumidity;
-
-  for(uint8_t i = ZONE_1; i <= ZONE_4; i++) {
-    currentZoneHumidity = checkZone(i);
-
-  }
-
-
 
 }
 
-
-// humidity values: 
-// 254 => 100%
-// 001 => 0%
-// 000 => error
-uint8_t checkZone(uint8_t zone) {
-  uint8_t zoneHumidity = 0;
-  analogRead()
-  return zoneHumidity;
+// initializes valve instance
+void OnOffDevice::init() {
+  pinMode(pin, OUTPUT);
 }
 
-void Valve::init() {
-  pinMode(valvePin, OUTPUT);
+void OnOffDevice::activate() {
+  digitalWrite(pin, activeLevel);
+  SET_BIT(IS_ACTIVE, flags);
 }
 
+void OnOffDevice::deactivate() {
+  digitalWrite(pin, !activeLevel);
+  RESET_BIT(IS_ACTIVE, flags);
+}
+
+// initializes sensor instance
 void Sensor::init() {
-  pinMode(sensorPin, INPUT);
+  pinMode(pin, INPUT);
+}
+
+// linear approximation of humidity curve
+// humidity values 
+// 255 => 100%
+// 000 => 0%
+void Sensor::getHumidity() {
+  humidity = analogRead(pin) * calibrationConstant;
+}
+
+void Sensor::calibration() {
+  // sensor in dry air (0%)
+  uint8_t dry = (analogRead(pin) / 1024);
+  
+  // wait for user input
+
+  // sensor submerged in water (100%)
+  uint8_t wet = (analogRead(pin) / 1024);
+
+  if(dry == wet){} // print error
+
+  // linear approximation of humidity curve
+  calibrationConstant = 255 / (wet - dry);
+}
+
+void Zone::getHumidity() {
+  humidity = 0;
+
 }
--- src/main.h
+++ src/main.h
@@ -5,6 +5,13 @@
 
 #define __AVR_ATmega328P__
 
+#ifdef __AVR_ATmega328P__
+    #define AREF 5
+#endif
+#ifdef ESP32
+    #define AREF 3.33f
+#endif
+
 // GPIO defines
 #define RELAY_1 1
 #define RELAY_2 2
@@ -26,47 +33,99 @@
 #define ZONE_3 3
 #define ZONE_4 4
 
+#define HIGH_ACTIVE 0x01
+#define LOW_ACTIVE  0x00
+
+#define SET_BIT(x, y)   (y |= x)
+#define RESET_BIT(x, y) (y &= ~x)
+
 // waterIO class
 class WaterIO {
 public:
-    waterIO();
+    WaterIO();
     static WaterIO& instance() {
         static WaterIO instance;
         return instance;
     }
 
+    Pump** pumps;
+
 // boot section
 void setup();
 void loop();
 void reset();
-
-// additional functions
 };
 
-class Valve {
+/*
+ Any identification numbers of objects shall only change during boot or when 
+ changing the settings
+*/
+
+enum deviceFlags: uint8_t {
+    IS_ACTIVE = (uint8_t) (1 << 0),
+    RESERVED = (uint8_t) (1 << 1),
+    RESERVED = (uint8_t) (1 << 2),
+    RESERVED = (uint8_t) (1 << 3),
+    RESERVED = (uint8_t) (1 << 4),
+    RESERVED = (uint8_t) (1 << 5),
+    RESERVED = (uint8_t) (1 << 6),
+    BYPASS_VALVE = (uint8_t) (1 << 7)
+};
+
+class OnOffDevice {
 public:
-    uint8_t valveNum;
-    uint8_t valvePin;
-    Zone** zoneChildren;
+    uint8_t id; // device id
+    uint8_t pin; // device gpio pin
+    uint8_t flags;
+    uint8_t activeLevel;
 
 void init();
+void activate();
+void deactivate();
 };
 
+// pumps
+class Pump: public OnOffDevice {
+public:
+    Valve** valves;
+    Zone** zones; // in case a pump is connected directly to one ore multiple zones
+    float flow;
+
+};
+
+// valves
+class Valve: public OnOffDevice {
+public:
+    Pump* pump;
+    Zone** zones;
+
+};
+
+// zones have no hardware
 class Zone {
 public:
-    uint8_t zoneNum;
-    uint8_t zonePin;
-    Sensor** sensorChildren;
-    Valve* valveParent;
+    uint8_t id;
+    Valve* valve;
+    Sensor** sensors;
+
+    uint8_t humidity;
 
 void init();
+void getHumidity();
 };
 
+// sensors for measuring humidity in one or multiple zones
 class Sensor {
 public:
-    uint8_t sensorNum;
-    uint8_t sensorPin;
-    Zone* zoneParent;
+    uint8_t id;
+    uint8_t pin;
+
+    Zone* zone;
+
+    uint8_t humidity;
+    double calibrationConstant;
 
 void init();
+void getHumidity();
+void calibration();
 };
